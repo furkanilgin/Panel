33ef9007b0ce5a50fde9e5cd2bfde2abcd32085e
FURKAN ILGIN <furkanilgin@gmail.com>
2014-02-21 21:30:21 +0200

    dataset is updated

--- controller/TestController.php
+++ controller/TestController.php
@@ -6,7 +6,7 @@ class TestController{
 
 	public function load(){
 		
-		$this->test->p_Panel->panelItemList[2]->dataset = array(
+		$this->test->p_Panel->panelItemList[1]->dataset = array(
 			array(1, './view/images/logo.png', 3),
 			array(4, './view/images/logo2.png', 6)
 		);
